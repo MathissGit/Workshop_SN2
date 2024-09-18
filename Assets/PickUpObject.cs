9b4e199734bf24cdf7bfa5f04150c027d72dd2aa
MathissGit <mathissineux@gmail.com>
2024-09-18 16:56:01 +0200

    code niveau city

---
+++ Assets/PickUpObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public AudioClip sound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
